c5a43766f6b5b08b4d616fa6f4cfd16800c5c114
mygenel <myg26@drexel.edu>
2020-07-03 19:00:42 -0400

    Added ability to log in

--- MoodAPI.py
+++ MoodAPI.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 users = {
-    "username": {"password": "pswd", "moods": []}
+    "username": {"password": "pswd", "moods": [], 'loggedIn': False}
 }
 
 
@@ -19,6 +19,18 @@ def mood():
             return jsonify({'mood': users[data['username']]['moods']}), 201
         else:
             return jsonify({'message': "There is no such user"}), 404
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    loginData = request.get_json()
+    username = loginData['username']
+    password = loginData['password']
+    if username in users and users[username]['password'] == password:
+        users[username]['loggedIn'] = True
+        return jsonify({'message': 'Login successful'}), 200
+    else:
+        return jsonify({'message': "Wrong username or password"}), 403
 
 
 if __name__ == '__main__':
