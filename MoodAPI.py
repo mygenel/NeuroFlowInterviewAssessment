fec707623e2d7f1571ee466cc31632d4c7426e9b
mygenel <myg26@drexel.edu>
2020-07-03 21:17:27 -0400

    Added functionality of GET request to get user mood list for logged in user. Added a function to calculate the streak of the user, and added the streak to the response.

--- MoodAPI.py
+++ MoodAPI.py
@@ -2,23 +2,42 @@ from datetime import date
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+
 users = {
     "username": {"password": "pswd", "moods": [], 'loggedIn': False}
 }
 
 
+def getStreak(moods):
+    if len(moods) <= 1:
+        return len(moods)
+    i = len(moods) - 2
+    streak = 1
+    while i >= 0 and (moods[i+1]['date'] - moods[i]['date']).days <= 1:
+        print((moods[i+1]['date'] - moods[i]['date']).days)
+        if (moods[i+1]['date'] - moods[i]['date']).days == 1:
+            streak += 1
+        i -= 1
+    return streak
+
+
 @app.route('/mood', methods=['GET', 'POST'])
 def mood():
-    if request.method == 'POST':
-        data = request.get_json()
-        username = data['username']
-        mood = data['mood']
-        if username in users:
+    data = request.get_json()
+    username = data['username']
+    if username in users:
+        if not users[username]['loggedIn']:
+            return jsonify({'message': 'User is not logged in'}), 403
+
+        if request.method == 'POST':
+            mood = data['mood']
             users[username]['moods'].append(
                 {'mood': mood, 'date': date.today()})
-            return jsonify({'mood': users[data['username']]['moods']}), 201
+            return jsonify({'mood': users[data['username']]['moods'], 'streak': getStreak(users[data['username']]['moods'])}), 201
         else:
-            return jsonify({'message': "There is no such user"}), 404
+            return jsonify({'mood': users[data['username']]['moods'], 'streak': getStreak(users[data['username']]['moods'])}), 201
+    else:
+        return jsonify({'message': "There is no such user"}), 404
 
 
 @app.route('/login', methods=['POST'])
@@ -34,4 +53,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
